915dc32c2a2378c7c57a10878440865398791c2f
Sarujan T <tsarujan2001@gmail.com>
2025-03-28 19:15:22 +0530

    feature-backend notification pages for student, admin, advisor were updated

---
+++ FOE_CourseRegistrationSystem/Views/Dashboard/Advisor/AdvisorNotification.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FOE_CourseRegistrationSystem.Views.Dashboard.Advisor
+{
+    public class AdvisorNotificationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ FOE_CourseRegistrationSystem/Views/Dashboard/Student/StudentNotification.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FOE_CourseRegistrationSystem.Views.Dashboard.Student
+{
+    public class StudentNotificationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
