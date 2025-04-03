aa28e8c45509c94d1dafd9d87f705ac8ab62eb07
NATANAlways <105733094+NATANAlways@users.noreply.github.com>
2025-04-04 03:08:00 +0530

    Add files via upload
    NotificationController added

---
+++ FOE_CourseRegistrationSystem/Controllers/NotificationController.cs
@@ -0,0 +1,49 @@
+﻿using FOE_CourseRegistrationSystem.Data;
+using FOE_CourseRegistrationSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FOE_CourseRegistrationSystem.Controllers
+{
+    public class NotificationController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NotificationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var userId = HttpContext.Session.GetInt32("UserID");
+            var userType = HttpContext.Session.GetString("UserType");
+
+            if (userId == null || userType == null)
+                return RedirectToAction("Login", "Account");
+
+            var notifications = await _context.Notifications
+                .Where(n => n.ReceiverID == userId &&
+                            n.ReceiverType.ToString() == userType)
+                .OrderByDescending(n => n.CreatedAt)
+                .ToListAsync();
+
+            return View(notifications);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> MarkAsRead(int id)
+        {
+            var notification = await _context.Notifications.FindAsync(id);
+            if (notification != null)
+            {
+                notification.IsRead = true;
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
